f5058bf319b8054cd9c2a2375d73d078c6fc9b7f
popfumo <90687009+popfumo@users.noreply.github.com>
2022-02-16 12:06:56 +0100

    Update hangman.hs

--- hangman.hs
+++ hangman.hs
@@ -1,28 +1,71 @@
-bubble :: [Integer] -> [Integer]
-bubble [] = []
-bubble [x] = [x]
-bubble (x:y:xs) =
-    if x > y
-        then y: bubble(x:xs)
-        else x: bubble(y:xs) 
+import System.IO 
+import Control.Monad
+import System.Random
 
-bubbleSort ::  [Integer] -> [Integer]
-bubbleSort [] = []
-bubbleSort lst = case bubble lst of
-    newList | newList == lst -> newList
-            | otherwise -> bubbleSort newList 
+type Guess = String
 
--- Variant = length  lst 
-bubbleSort2 [] = []
-bubbleSort2 lst = 
-    let newList = bubble lst 
-    in bubbleSort2 (init newList) ++ [last newList] 
+type Word = String
 
---bubbleSort2 [1,2,3]
---lst == [1,2,3]
--- x == 1
--- xs = [2,3]
+main :: IO ()
+main = do
+    putStrLn "   ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄    ▄  \n\
+              \  █  █ █  █      █  █  █ █       █  █▄█  █      █  █  █ █\n\
+              \  █  █▄█  █  ▄   █   █▄█ █   ▄▄▄▄█       █  ▄   █   █▄█ █\n\
+              \  █       █ █▄█  █       █  █  ▄▄█       █ █▄█  █       █\n\
+              \  █   ▄   █      █  ▄    █  █ █  █       █      █  ▄    █\n\
+              \  █  █ █  █  ▄   █ █ █   █  █▄▄█ █ ██▄██ █  ▄   █ █ █   █\n\
+              \  █▄▄█ █▄▄█▄█ █▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄█   █▄█▄█ █▄▄█▄█  █▄▄█\n\
+              \     _______                                             \n\
+              \     |     |                                             \n\ 
+              \     |     O                                             \n\
+              \     |    -|-                                            \n\
+              \   __|__  J L                                            \n\
+              \   |___|                                                 "
+              
+              
 
 
---lst@(x:xs) = 
-    --bubble lst 
+
+--Main function
+    --Monad.
+
+--Check gamestate
+    --Win/Lose
+    --New game, calls on the main function again. 
+    --Quit game, terminates the main function.
+
+--Guess
+    --User wordinput.
+    --Connected to guess count.
+    --Fixed amount of guesses.
+
+filepath = "wordlist.txt"
+tpath = "testfile.txt"
+
+
+randomWord = do  
+        handle <- openFile filepath ReadMode -- Import wordlist
+        contents <- hGetContents handle      -- Parse content in wordlist
+        let list = lines contents            -- Creats a list of every word
+            upper = length list              -- Creates a upper bound with the help of length of list
+        ranInt <- randomRIO (1,upper-1)      -- Returns a random IO Int
+        return $ list !! ranInt              -- Returns a random word from the list with the help of the random number we get.
+        hClose handle                        -- closes handle
+
+
+    --txt-file. (or parse the file into an array)
+    --Plot out length of word in GUI. 
+
+--WordCheck
+    --Keeps track if the word is complete, calls on win status if the word is complete. 
+
+--Guess count
+    --Keeps track of how many guesses.
+    --Wrong or right guess.
+    --Max amount of guesses, if guess limit exceed: calls on lose status. 
+
+--Data types
+    --Letter, a list of Chars.
+
+
+
