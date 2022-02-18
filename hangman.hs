0cb00399e55e78d971106b23572cb1d858fafd73
unknown <edvard@axelman.se>
2022-02-18 17:04:31 +0100

    added multiple functions

--- hangman.hs
+++ hangman.hs
@@ -6,8 +6,11 @@ import Control.Monad
 type Guess = String
 type Word = String
 
-main :: IO ()
-main = do
+
+filepath = "wordlist.txt"
+
+splash :: IO ()
+splash = do
     putStrLn "   ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄    ▄  \n\
               \  █  █ █  █      █  █  █ █       █  █▄█  █      █  █  █ █\n\
               \  █  █▄█  █  ▄   █   █▄█ █   ▄▄▄▄█       █  ▄   █   █▄█ █\n\
@@ -22,8 +25,89 @@ main = do
               \   __|__  J L                                            \n\
               \   |___|                                                 "
     putStrLn "Written by Erik Odhner, Edvard Axelman and Viktor Wallstén"
-    word <- getLine
-    putStrLn $ "The word " ++ word ++ " is not correct, kys"        
+
+main :: IO ()
+main = do
+    splash
+    menu
+
+theword = randomWord 
+
+menu :: IO ()
+menu = do
+    putStrLn "--------------------"
+    putStrLn "1. Singleplayer"
+    putStrLn "2. Multiplayer"
+    putStrLn "3. Quit game"
+    putStrLn "--------------------"
+    option <- getLine 
+    menuChoice option
+
+menuChoice :: IO String -> IO ()
+menuChoice option = do
+    case option of 
+        "1" -> singleGame
+        "2" -> multiGame
+        "3" -> return ()
+         -- _  -> menu TODO!!!!!!!!
+
+
+
+randomWord :: IO String
+randomWord = do  
+        handle <- openFile filepath ReadMode -- Import wordlist
+        contents <- hGetContents handle      -- Parse content in wordlist
+        let list = lines contents            -- Creats a list of every word
+            upper = length list              -- Creates a upper bound with the help of length of list
+        ranInt <- randomRIO (1,upper-1)      -- Returns a random IO Int
+        hClose handle                        -- closes handle
+        return $ list !! ranInt              -- Returns a random word from the list with the help of the random number we get.
+                               
+
+
+
+
+test = do
+    rword <- randomWord
+    putStrLn ("Edvard is a: " ++ rword)
+
+
+--singleGame :: IO ()
+singleGame = do
+    rword <- randomWord
+    getGuess
+
+
+guessCount = undefined 
+    --if guess is wrong. Add 1 to acc.
+    --There is a guess limit. So when the acc reaches a certain Int. Call lose function. 
+
+multiGame = undefined
+
+win :: IO ()
+win = do 
+    putStrLn "Congratulations! You won! faggot"
+    endgame 
+
+lose :: IO ()
+lose = do
+    rword <- randomWord
+    putStrLn ("Out of guesses! The correct word was: " ++ rword)
+    endgame
+
+
+endgame :: IO ()
+endgame = do
+    putStrLn "Do you want play again? (y/n): "
+    usrInp <- getLine 
+    if usrInp == "yes" || usrInp == "y"
+        then main
+        else return ()
+
+            
+--När array är tom når vi basfallet och då har användaren gissat rätt.
+--Plocka ur ett
+
 --Main function
     --Monad.
 
@@ -33,22 +117,26 @@ main = do
     --Quit game, terminates the main function.
 
 --Guess
+
+
+getGuess = do
+    putStrLn "What is your next guess?"
+    inputGuess <- getLine
+    case inputGuess of
+        -- om tom gissning
+        [] -> getGuess
+        x  -> return x
+        -- om det är en string
+        --(x : tail) -> return x
+
+
+
+    -- validChar -- TODO Check that input is a letter and also either only 1 char or the whole string
+
+
     --User wordinput.
     --Connected to guess count.
     --Fixed amount of guesses.
-
-filepath = "wordlist.txt"
-
-randomWord = do  
-        handle <- openFile filepath ReadMode -- Import wordlist
-        contents <- hGetContents handle      -- Parse content in wordlist
-        let list = lines contents            -- Creats a list of every word
-            upper = length list              -- Creates a upper bound with the help of length of list
-        ranInt <- randomRIO (1,upper-1)      -- Returns a random IO Int
-        return $ list !! ranInt              -- Returns a random word from the list with the help of the random number we get.
-        hClose handle                        -- closes handle
-
-
 
     --txt-file. (or parse the file into an array)
     --Plot out length of word in GUI. 
